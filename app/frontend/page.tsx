62dbb1b45627d176981537624acf3e8ecc597401
bhavc <bhavdip.dev@gmail.com>
2023-10-06 13:26:58 -0400

    initial project scaffold

---
+++ app/frontend/page.tsx
@@ -0,0 +1,9 @@
+export default function Frontend() {
+	return (
+		<main className="">
+			<div className="mt-2">
+				<h1 className="text-3xl text-center">Backend</h1>
+			</div>
+		</main>
+	);
+}
